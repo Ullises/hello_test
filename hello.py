c9973172a37ac51809e6355383e34e7b4449bdbb
Ullises <uli.ulmp@gmail.com>
2024-05-12 00:17:47 -0600

    counting words func added

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
+import re
+
 def factorial(n):
     if n == 0:
-        return 1
+        return "HelloWorld "
     else:
     	return n * factorial(n - 1)
-    	
+	
 number = 5
 result = factorial(number)
-print(f"The factorial of {number} is {result}.")
+print(f"The factorial of {number} Hello World(s) is {result}.")
+
+
+def count_words(text):
+    """
+    This function counts the number of words in a string
+    """
+    # Split the text into a list of words by white spaces
+    words = text.split()
+
+    #Returns the length of the list of words
+    return len(words)
+
+#Example use
+text = result
+word_count = count_words(text)
+print(f"The text has {word_count} words.")
